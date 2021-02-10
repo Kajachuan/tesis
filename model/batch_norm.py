ef03eca63fc38ea899d960c7d62e5bc029a101bf
Kajachuan <kevincajachuan@hotmail.com>
2021-02-10 16:04:58 -0300

    Cambios en el BatchNorm

--- model/batch_norm.py
+++ model/batch_norm.py
@@ -1,41 +1,34 @@
+import torch
 import torch.nn as nn
 
 class BatchNorm(nn.Module):
     """
     Capa de normalización por lote (batch)
-
-    Argumentos:
-        num_features {int} -- Argumento num_features de BatchNorm1d
-        feature_dim {int} -- Dimensión que será normalizada
-        **kwargs {dict} -- Argumentos de BatchNorm1d
     """
-
-    def __init__(self, num_features=1, feature_dim=2, **kwargs):
-        super(BatchNorm, self).__init__()
-        self.num_features = num_features
-        self.feature_dim = feature_dim
-        self.batch_norm = nn.BatchNorm1d(num_features, **kwargs)
-
-    def forward(self, data):
+    def __init__(self, **kwargs) -> None:
         """
         Argumentos:
-            data {torch.Tensor} -- Entrada que será normalizada. Dim: (B, T, F, C)
+            **kwargs {dict} -- Argumentos de BatchNorm1d
+        """
+        super(BatchNorm, self).__init__()
+        self.batch_norm = nn.BatchNorm1d(**kwargs)
+
+    def forward(self, data: torch.Tensor) -> torch.Tensor:
+        """
+        Argumentos:
+            data {torch.Tensor} -- Entrada que será normalizada de dimensión
+                                   (n_batch, n_channels, n_bins, n_frames)
 
         Retorna:
-            torch.Tensor -- Entrada normalizada
-
-        Referencia:
-            B: Tamaño del lote (batch)
-            T: Número de bins de tiempo
-            F: Número de bins de frecuencia
-            C: Número de canales (mono, stereo, etc.)
+            Entrada normalizada de dimensión (n_batch, n_channels, n_bins, n_frames)
         """
-        data = data.transpose(self.feature_dim, 1) # Dim: (B, F, T, C)
-        shape = data.shape
-        new_shape = (shape[0], self.num_features, -1)
 
-        data = data.reshape(new_shape) # Dim: (B, num_features, -1)
-        data = self.batch_norm(data) # Dim: (B, num_features, -1)
-        data = data.reshape(shape) # Dim: (B, F, T, C)
-        data = data.transpose(self.feature_dim, 1) # Dim: (B, T, F, C)
+        data = data.transpose(1, 2) # Dim: (n_batch, n_bins, n_channels, n_frames)
+        shape = data.shape
+        new_shape = ()
+
+        data = data.reshape(shape[0], shape[1], -1) # Dim: (n_batch, n_bins, *)
+        data = self.batch_norm(data) # Dim: (n_batch, n_bins, *)
+        data = data.reshape(shape) # Dim: (n_batch, n_bins, n_channels, n_frames)
+        data = data.transpose(1, 2) # Dim: (n_batch, n_channels, n_bins, n_frames)
         return data
