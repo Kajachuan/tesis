81f737ec4d2c392422e586856ba3d82c16838434
Kajachuan <kevincajachuan@hotmail.com>
2021-02-10 16:56:20 -0300

    Cambios en el BLSTM

--- model/blstm.py
+++ model/blstm.py
@@ -2,51 +2,38 @@ import torch.nn as nn
 
 class BLSTM(nn.Module):
     """
-    Crea varias capas BLSTM
-
-    Argumentos:
-        num_features {int} -- Número de features
-        hidden_size {int} -- Cantidad de unidades en cada capa (tamaño de las capas ocultas)
-        num_layers {int} -- Número de capas BLSTM
-        dropout {float} -- Dropout entre capas
+    Crea una pila de capas BLSTM
     """
-    def __init__(self, num_features, hidden_size, num_layers, dropout):
+    def __init__(self, num_features: int, hidden_size: int, num_layers: int, dropout: float):
+        """
+        Argumentos:
+            num_features -- Número de features
+            hidden_size -- Cantidad de unidades en cada capa (tamaño de las capas ocultas)
+            num_layers -- Número de capas BLSTM
+            dropout -- Dropout entre capas
+        """
         super(BLSTM, self).__init__()
         self.blstm = nn.LSTM(num_features, hidden_size, num_layers, batch_first=True,
                              dropout=dropout, bidirectional=True)
 
         # Inicialización
-        for name, param in self.blstm.named_parameters():
-            if 'bias' in name:
-                nn.init.constant_(param, 0.0)
-            elif 'weight' in name:
-                nn.init.xavier_normal_(param) # Revisar si se podría cambiar
+        # for name, param in self.blstm.named_parameters():
+        #     if 'bias' in name:
+        #         nn.init.constant_(param, 0.0)
+        #     elif 'weight' in name:
+        #         nn.init.xavier_normal_(param)
 
-        # Revisar que hace esto
-        for names in self.blstm._all_weights:
-            for name in filter(lambda n: 'bias' in n, names):
-                bias = getattr(self.blstm, name)
-                n = bias.size(0)
-                start, end = n // 4, n // 2
-                bias.data[start:end].fill_(1.)
-
-    def forward(self, data):
+    def forward(self, data: torch.Tensor):
         """
         Argumentos:
-            data {torch.Tensor} -- Entrada de dimensión (B, T, F, C)
+            data -- Entrada de dimensión (n_batch, n_channels, n_bins, n_frames)
 
         Retorna:
-            torch.Tensor -- Salida de dimensión (B, T, H)
-
-        Referencia:
-            B: Tamaño del lote (batch)
-            T: Número de bins de tiempo
-            F: Número de bins de frecuencia
-            C: Número de canales (mono, stereo, etc.)
-            H: Cantidad de unidades (hidden size)
+            torch.Tensor -- Salida de dimensión (n_batch, n_frames, hidden_size)
         """
+        data = data.transpose(1, 3) # Dim: (n_batch, n_frames, n_bins, n_channels)
         shape = data.shape
-        data = data.reshape(shape[0], shape[1], -1) # Dim: (B, T, -1)
+        data = data.reshape(shape[0], shape[1], -1) # Dim: (n_batch, n_frames, *)
         self.blstm.flatten_parameters()
-        data = self.blstm(data)[0] # Dim: (B, T, H)
+        data = self.blstm(data)[0] # Dim: (n_batch, n_frames, hidden_size)
         return data
