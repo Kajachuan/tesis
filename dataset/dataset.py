68b2b0a565238d0b4c585e409562eeea095f44dd
Kajachuan <kevincajachuan@hotmail.com>
2021-02-23 21:39:23 -0300

    Cambio nombre del argumento para mejor comprensión

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -9,14 +9,14 @@ class MUSDB18Dataset(Dataset):
     Dataset MUSDB18 (basado en la implementación en OpenUnmix)
     """
     def __init__(self, base_path: str, subset: str, split: str, target: str,
-                 duration: Optional[float], divisions: int = 1, random: bool = False) -> None:
+                 duration: Optional[float], samples: int = 1, random: bool = False) -> None:
         """
         base_path -- Ruta del dataset
         subset -- Nombre del conjunto: 'train' o 'test'
         split -- División del entrenamiento: 'train' o 'valid' cuando subset='train'
         target -- Instrumento que se va a separar 'vocals', 'drums', 'bass' o 'vocal'
         duration -- Duración de cada canción en segundos
-        divisions -- En cuantas partes se va a dividir una canción
+        samples -- Cantidad de muestras de cada cancion
         random -- True si se van a mezclar las canciones de forma aleatoria
         """
         super(MUSDB18Dataset, self).__init__()
@@ -24,7 +24,7 @@ class MUSDB18Dataset(Dataset):
         self.split = split
         self.target = target
         self.duration = duration
-        self.divisions = divisions
+        self.samples = samples
         self.random = random
         self.mus = musdb.DB(root=base_path, subsets=subset, split=split)
 
@@ -32,7 +32,7 @@ class MUSDB18Dataset(Dataset):
         sources = []
         target_idx = None
 
-        track = self.mus[index // self.divisions]
+        track = self.mus[index // self.samples]
 
         if self.split == 'train' and self.duration:
             for idx, source in enumerate(self.mus.setup['sources']):
@@ -61,4 +61,4 @@ class MUSDB18Dataset(Dataset):
         return x, y
 
     def __len__(self) -> int:
-        return len(self.mus) * self.divisions
+        return len(self.mus) * self.samples
