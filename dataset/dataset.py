8e077d5b54286cb76fb4653821a4ef1ac70fd261
Kajachuan <kevincajachuan@hotmail.com>
2021-02-26 10:35:21 -0300

    Elimino el cambio de volumen

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -46,7 +46,6 @@ class MUSDB18Dataset(Dataset):
                 track.chunk_start = random.uniform(0, track.duration - self.duration)
 
                 audio = torch.as_tensor(track.sources[source].audio.T, dtype=torch.float32)
-                audio *= random.uniform(0.25, 1.25)
                 sources.append(audio)
 
             stems = torch.stack(sources, dim=0)
