72b1c9ff8adfc641810e7b5e6da6833523ad1f32
Kajachuan <kevincajachuan@hotmail.com>
2021-03-07 12:54:31 -0300

    Agrego archivo de evaluacion

---
+++ evaluate.py
@@ -0,0 +1,38 @@
+import argparse
+import musdb
+import museval
+import torch
+import tqdm
+from model.model import Model
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint", type=str, help="Checkpoint del modelo a evaluar")
+    parser.add_argument("--root", type=str, help="Ruta del dataset")
+    parser.add_argument("--target", type=str, help="Instrumento a separar")
+    args = parser.parse_args()
+
+    mus = musdb.DB(root=args.root, subsets='test')
+
+    use_cuda = torch.cuda.is_available()
+    print("GPU disponible:", use_cuda)
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    state = torch.load(args.checkpoint)
+    network = Model(*state["args"]).to(device)
+    network.load_state_dict(state["state_dict"])
+    network.eval()
+
+    results = museval.EvalStore()
+    with torch.no_grad():
+        for track in tqdm.tqdm(mus):
+            signal = torch.as_tensor(track.audio.T).to(device)
+            _, estimate = network(signal.unsqueeze(0))
+            estimate = estimate.cpu()[0, ...]
+            score = museval.eval_mus_track(track.targets[args.target].audio.T, estimate)
+            results.add_track(score)
+
+    print(results)
+
+if __name__ == '__main__':
+    main()
