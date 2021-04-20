fd465511bfa2bcc4ab9a84a60d389cb9a0a171a1
Kajachuan <kevincajachuan@hotmail.com>
2021-04-20 18:14:42 -0300

    Cambio el script de evaluación

--- evaluate.py
+++ evaluate.py
@@ -6,34 +6,39 @@ import tqdm
 from model.model import Model
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint", type=str, help="Checkpoint del modelo a evaluar")
-    parser.add_argument("--root", type=str, help="Ruta del dataset")
-    parser.add_argument("--target", type=str, help="Instrumento a separar")
-    args = parser.parse_args()
+    # Solo musdb por ahora !!
 
-    mus = musdb.DB(root=args.root, subsets='test')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoints", type=str, help="Ruta del modelo a evaluar")
+    parser.add_argument("--init", type=int, default=0, choices=range(50), help="Índice de la canción de inicio")
+    parser.add_argument("--output", type=str, help="Ruta donde se guarda la evaluación")
+    parser.add_argument("--root", type=str, help="Ruta del dataset")
+    args = parser.parse_args()
 
     use_cuda = torch.cuda.is_available()
     print("GPU disponible:", use_cuda)
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    state = torch.load(args.checkpoint)
-    network = Model(*state["args"]).to(device)
-    network.load_state_dict(state["state_dict"])
-    network.eval()
+    stems = ["vocals", "drums", "bass", "other"]
+    models = {}
+    print("Cargando modelos")
+    for stem in stems:
+        state = torch.load(f"{args.checkpoints}/{stem}")
+        models[stem] = Model(*state["args"]).to(device)
+        models[stem].load_state_dict(state["state_dict"])
+        models[stem].eval()
 
-    results = museval.EvalStore()
-    with torch.no_grad():
-        for track in tqdm.tqdm(mus.tracks):
-            signal = torch.as_tensor(track.audio.T, dtype=torch.float32).to(device)
-            _, estimate = network(signal.unsqueeze(0))
-            vocals = estimate.cpu().numpy()[0, ...].T
-            estimates = {args.target: vocals, 'accompaniment': track.audio - vocals}
-            score = museval.eval_mus_track(track, estimates)
-            results.add_track(score)
+    print("Cargando canciones de test")
+    mus = musdb.DB(root=args.root, subsets='test')
 
-    print(results)
+    for i in tqdm.tqdm(range(args.init, 50)):
+        track = mus.tracks[i]
+        print(f"Canción {i}: {track.name}")
+        signal = torch.as_tensor(track.audio.T, dtype=torch.float32).to(device)
+        result = {}
+        for stem in stems:
+            result[stem] = models[stem](signal.unsqueeze(0)).cpu().numpy()[0, ...].T
+        museval.eval_mus_track(track, result, f"{args.output}/{track.name}")
 
 if __name__ == '__main__':
     main()
