ec5c15a7451ad1d1ab2b3f90b5e8aada386f48be
Kajachuan <kevincajachuan@hotmail.com>
2021-04-26 17:36:26 -0300

    Agrego una clase separador

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,7 @@ import musdb
 import museval
 import torch
 import tqdm
-from model.model import Model
+from separator import *
 
 def main():
     # Solo musdb por ahora !!
@@ -11,23 +11,21 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoints", type=str, help="Ruta del modelo a evaluar")
     parser.add_argument("--init", type=int, default=0, choices=range(50), help="Índice de la canción de inicio")
+    parser.add_argument("--model", type=str, choices=["spectrogram", "wave"], help="Modelo a utilizar")
+    parser.add_argument("--other", action="store_true", help="Utilizar el modelo de other")
     parser.add_argument("--output", type=str, help="Ruta donde se guarda la evaluación")
     parser.add_argument("--root", type=str, help="Ruta del dataset")
+    parser.add_argument("--vocals", action="store_true", help="Restar vocals para calcular el acompañamiento")
     args = parser.parse_args()
 
     use_cuda = torch.cuda.is_available()
     print("GPU disponible:", use_cuda)
     device = torch.device("cuda:0" if use_cuda else "cpu")
 
-    stems = ["vocals", "drums", "bass", "other"]
-    models = {}
-    print("Cargando modelos:")
-    for stem in stems:
-        print(f"\tCargando modelo de {stem}")
-        state = torch.load(f"{args.checkpoints}/{stem}/best_checkpoint")
-        models[stem] = Model(*state["args"]).to(device)
-        models[stem].load_state_dict(state["state_dict"])
-        models[stem].eval()
+    if args.model == "spectrogram":
+        separator = SpectrogramSeparator(self.checkpoints, self.other, self.vocals)
+    else: # wave
+        raise NotImplementedError
 
     print("Cargando canciones de test")
     mus = musdb.DB(root=args.root, subsets='test')
@@ -36,10 +34,7 @@ def main():
         track = mus.tracks[i]
         print(f"Canción {i}: {track.name}")
         signal = torch.as_tensor(track.audio.T, dtype=torch.float32).to(device)
-        result = {}
-        for stem in stems:
-            _, _, estim = models[stem](signal.unsqueeze(0))
-            result[stem] = estim[0, ...].cpu().detach().numpy().T
+        result = separator.separate(signal)
         museval.eval_mus_track(track, result, f"{args.output}")
 
 if __name__ == '__main__':
---
+++ separator.py
@@ -0,0 +1,61 @@
+import torch
+from typing import Dict
+from numpy import ndarray
+from model.model import Model
+
+class Separator:
+    def __init__(self, root: str) -> None:
+        """
+        Argumentos:
+            root -- Directorio donde se encuentran los checkpoints
+        """
+        self.stems = ["vocals", "drums", "bass"]
+        self.models = {}
+        for stem in self.stems:
+            self._load_model(stem)
+
+    def load_model(self, stem: str) -> None:
+        """
+        Carga el modelo de un stem determinado al separador
+
+        Argumentos:
+            stem -- Nombre del stem
+        """
+        print(f"Cargando modelo de {stem}")
+        state = torch.load(f"{root}/{stem}/best_checkpoint")
+        self.models[stem] = Model(*state["args"]).to(device)
+        self.models[stem].load_state_dict(state["state_dict"])
+        self.models[stem].eval()
+
+class SpectrogramSeparator(Separator):
+    def __init__(self, root: str, use_other: bool, use_vocals: bool) -> None:
+        """
+        Argumentos:
+            root -- Directorio donde se encuentran los checkpoints
+            use_other -- True si se utiliza el modelo entrenado para "other"
+                         False si se calcula "other" como la resta con "vocals" + "drums" + "bass"
+            use_vocals -- True si se calcula "accompaniment" como la resta con "vocals"
+                          False si se calcula "accompaniment" como "drums" + "bass" + "other"
+        """
+        super(SpectrogramSeparator, self).__init__(root)
+        self.use_other = use_other
+        self.use_vocals = use_vocals
+
+        if self.use_other:
+            self.stems.append("other")
+            self.load_model("other")
+
+    def separate(self, track: torch.Tensor) -> Dict[str, ndarray]:
+        result = {}
+        for stem in self.stems:
+            _, _, estim = models[stem](track.unsqueeze(0))
+            result[stem] = estim[0, ...].cpu().detach().numpy().T
+        track = track.cpu().detach().numpy().T
+
+        if not self.use_other:
+            result["other"] = track - (result["vocals"] + result["drums"] + result["bass"])
+
+        if self.use_vocals:
+            result["accompaniment"] = track - result["vocals"]
+        else:
+            result["accompaniment"] = result["drums"] + result["bass"] + result["other"]
