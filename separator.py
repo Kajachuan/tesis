fa3b182f96da58c6650e55d3cd05c27c489d5a07
Kajachuan <kevincajachuan@hotmail.com>
2021-04-27 15:06:27 -0300

    Modifico el separador

--- separator.py
+++ separator.py
@@ -1,33 +1,11 @@
 import torch
+from torch.nn import Module
 from typing import Dict
 from numpy import ndarray
-from model.model import Model
+from spectrogram_model.model import SpectrogramModel
+from wave_model.model import WaveModel
 
-class Separator:
-    def __init__(self, root: str) -> None:
-        """
-        Argumentos:
-            root -- Directorio donde se encuentran los checkpoints
-        """
-        self.stems = ["vocals", "drums", "bass"]
-        self.models = {}
-        for stem in self.stems:
-            self._load_model(stem)
-
-    def load_model(self, stem: str) -> None:
-        """
-        Carga el modelo de un stem determinado al separador
-
-        Argumentos:
-            stem -- Nombre del stem
-        """
-        print(f"Cargando modelo de {stem}")
-        state = torch.load(f"{root}/{stem}/best_checkpoint")
-        self.models[stem] = Model(*state["args"]).to(device)
-        self.models[stem].load_state_dict(state["state_dict"])
-        self.models[stem].eval()
-
-class SpectrogramSeparator(Separator):
+class SpectrogramSeparator:
     def __init__(self, root: str, use_other: bool, use_vocals: bool) -> None:
         """
         Argumentos:
@@ -37,13 +15,17 @@ class SpectrogramSeparator(Separator):
             use_vocals -- True si se calcula "accompaniment" como la resta con "vocals"
                           False si se calcula "accompaniment" como "drums" + "bass" + "other"
         """
-        super(SpectrogramSeparator, self).__init__(root)
+        self.root = root
         self.use_other = use_other
         self.use_vocals = use_vocals
 
+        self.stems = ["vocals", "drums", "bass"]
         if self.use_other:
             self.stems.append("other")
-            self.load_model("other")
+
+        self.models = {}
+        for stem in self.stems:
+            self.load_model(stem)
 
     def separate(self, track: torch.Tensor) -> Dict[str, ndarray]:
         result = {}
@@ -59,3 +41,12 @@ class SpectrogramSeparator(Separator):
             result["accompaniment"] = track - result["vocals"]
         else:
             result["accompaniment"] = result["drums"] + result["bass"] + result["other"]
+
+        return result
+
+    def load_model(self, stem: str) -> None:
+        print(f"Cargando modelo de {stem}")
+        state = torch.load(f"{self.root}/{stem}/best_checkpoint")
+        self.models[stem] = SpectrogramModel(*state["args"]).to(device)
+        self.models[stem].load_state_dict(state["state_dict"])
+        self.models[stem].eval()
