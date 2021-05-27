d524baae88efc98beb507ad5761986499f681866
Kajachuan <kevincajachuan@hotmail.com>
2021-05-27 15:20:53 -0300

    Se evalúan TODAS las canciones divididas en 2

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,32 @@
 import argparse
+import json
 import musdb
 import museval
+import os
 import torch
 import tqdm
 from separator import *
+
+def merge_json(out_dir: str, track_name: str) -> None:
+    with open(f"{out_dir}1/{track_name}") as f1, open(f"{out_dir}2/{track_name}") as f2:
+        data1 = json.load(f1)
+        data2 = json.load(f2)
+
+    for target1 in data1["targets"]:
+        for target2 in data2["targets"]:
+            if target2["name"] == target1["name"]:
+                frames1 = target1["frames"]
+                frames2 = target2["frames"]
+                for frame in frames2:
+                    frame["time"] += len(frames1)
+                target1["frames"] = frames1 + frames2
+                break
+
+    with open(out_dir, "w") as out:
+        json.dump(data1, out, indent=2)
+
+    os.remove(f"{out_dir}1/{track_name}")
+    os.remove(f"{out_dir}2/{track_name}")
 
 def main():
     # Solo musdb por ahora !!
@@ -25,7 +48,9 @@ def main():
 
     if args.model == "spectrogram":
         separator = SpectrogramSeparator(args.checkpoints, args.other, args.vocals, device)
-    else: # wave
+    elif args.model == "wave":
+        separator = WaveSeparator(args.checkpoints, args.other, args.vocals, device)
+    else:
         raise NotImplementedError
 
     print("Cargando canciones de test")
@@ -34,9 +59,27 @@ def main():
     for i in tqdm.tqdm(range(args.init, args.end + 1)):
         track = mus.tracks[i]
         print(f"Canción {i}: {track.name}")
+
+        # Primera mitad
+        print("Primera mitad")
+        half = track.duration // 2
+        track.chunk_duration = half
         signal = torch.as_tensor(track.audio.T, dtype=torch.float32).to(device)
         result = separator.separate(signal)
-        museval.eval_mus_track(track, result, f"{args.output}")
+        museval.eval_mus_track(track, result, f"{args.output}1")
+
+        # Segunda mitad
+        print("Segunda mitad")
+        track.chunk_start = half
+        track.chunk_duration = track.duration - half
+        signal = torch.as_tensor(track.audio.T, dtype=torch.float32).to(device)
+        result = separator.separate(signal)
+        museval.eval_mus_track(track, result, f"{args.output}2")
+
+        merge_json(args.output, track.name)
+
+    os.rmdir(f"{args.output}1")
+    os.rmdir(f"{args.output}2")
 
 if __name__ == '__main__':
     main()
