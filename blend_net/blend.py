51d089e27e77371a305a35b93e32a3c6bb6209cb
Kajachuan <kevincajachuan@hotmail.com>
2021-05-29 20:05:33 -0300

    Cambio la mezcla para no guardar los modelos anteriores

--- blend_net/blend.py
+++ blend_net/blend.py
@@ -7,23 +7,18 @@ class BlendNet(nn.Module):
     """
     Modelo de mezcla de modelos de espectrograma y wave
     """
-    def __init__(self, stft_path: str, wave_path: str, device: torch.device) -> None:
+    def __init__(self, channels: int, nfft: int, hop: int) -> None:
         """
         Argumentos:
-            stft_path -- Ruta del checkpoint del modelo de STFT
-            wave_path -- Ruta del checkpoint del modelo de Wave
-            device -- Device de PyTorch
+            channels -- Número de canales de audio
+            nfft -- Número de puntos para calcular la nfft
+            hop -- Número de puntos de hop
         """
         super(BlendNet, self).__init__()
-
-        stft_state = torch.load(f"{stft_path}/best_checkpoint", map_location=device)
-        wave_state = torch.load(f"{wave_path}/best_checkpoint", map_location=device)
-
-        stft_args = stft_state["args"]
-        self.channels = stft_args[0]
-        self.nfft = stft_args[-2]
+        self.channels = channels
+        self.nfft = nfft
         self.bins = self.nfft // 2 + 1
-        self.hop = stft_args[-1]
+        self.hop = hop
 
         self.padding = nn.ZeroPad2d((0, self.nfft, 0, 0))
         self.conv = nn.Conv1d(in_channels=self.channels,
@@ -34,28 +29,19 @@ class BlendNet(nn.Module):
                                          out_channels=self.channels,
                                          kernel_size=self.nfft,
                                          stride=self.hop)
+        self.linear = nn.Linear(in_features=3 * self.channels, out_features=self.channels)
 
-        self.stft_model = SpectrogramModel(*stft_args).to(device)
-        self.stft_model.load_state_dict(stft_state["state_dict"])
-        self.stft_model.eval()
-
-        self.wave_model = WaveModel(*wave_state["args"]).to(device)
-        self.wave_model.load_state_dict(wave_state["state_dict"])
-        self.wave_model.eval()
-
-    def forward(self, data: torch.Tensor) -> torch.Tensor:
+    def forward(self, stft: torch.Tensor, wave_stft: torch.Tensor, wave: torch.Tensor) -> torch.Tensor:
         """
         Argumentos:
-            data -- Señales de audio de dimensión (n_batch, n_channels, timesteps)
+            stft -- STFT de dicho modelo de dimensión (n_batch, n_channels, n_bins, n_frames)
+            wave_stft -- Audio recuperado de la STFT de dimensión (n_batch, n_channels, timesteps)
+            wave -- Audio del modelo de Wave de dimensión (n_batch, n_channels, timesteps)
 
         Retorna:
             Separación de dimensión (n_batch, n_channels, timesteps)
         """
-        with torch.no_grad():
-            _, stft, _ = self.stft_model(data)
-            wave = self.wave_model(data)
-
-        timesteps = data.size(-1)
+        timesteps = wave.size(-1)
 
         data = self.padding(wave)                                           # Dim: (n_batch, n_channels, timesteps + nfft)
         data = self.conv(data)                                              # Dim: (n_batch, n_channels * n_bins, n_frames)
@@ -64,4 +50,10 @@ class BlendNet(nn.Module):
         data = data.reshape(data.size(0), -1, data.size(-1))                # Dim: (n_batch, 2 * n_channels * n_bins, n_frames)
         data = self.deconv(data)                                            # Dim: (n_batch, n_channels, out_length)
         data = data[..., :timesteps]                                        # Dim: (n_batch, n_channels, timesteps)
+
+        data = torch.stack([data, wave_stft, wave], dim=-1)                 # Dim: (n_batch, n_channels, timesteps, 3)
+        data = data.transpose(1, 2)                                         # Dim: (n_batch, timesteps, n_channels, 3)
+        data = data.reshape(data.size(0), data.size(1), -1)                 # Dim: (n_batch, timesteps, 3 * n_channels)
+        data = self.linear(data)                                            # Dim: (n_batch, timesteps, n_channels)
+        data = data.transpose(1, 2)                                         # Dim: (n_batch, n_channels, timesteps)
         return data
---
+++ train_blend.py
@@ -0,0 +1,157 @@
+import argparse
+import os
+import torch
+import tqdm
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from torch.nn.functional import mse_loss
+from dataset.dataset import MUSDB18Dataset
+from spectrogram_model.model import SpectrogramModel
+from wave_model.model import WaveModel
+from blend_net.blend import BlendNet
+
+def train(network, train_loader, device, stft_model, wave_model, optimizer):
+    batch_loss, count = 0, 0
+    network.train()
+    pbar = tqdm.tqdm(train_loader)
+    for x, y in pbar:
+        pbar.set_description("Entrenando batch")
+        x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+        optimizer.zero_grad()
+
+        with torch.no_grad():
+            _, stft, wave_stft = stft_model(x)
+            wave = wave_model(x)
+
+        y_hat = network(stft, wave_stft, wave)
+        loss = mse_loss(y_hat, y)
+        loss.backward()
+        optimizer.step()
+        batch_loss += loss.item() * y.size(0)
+        count += y.size(0)
+    return batch_loss / count
+
+def valid(network, valid_loader, device, stft_model, wave_model):
+    batch_loss, count = 0, 0
+    network.eval()
+    with torch.no_grad():
+        pbar = tqdm.tqdm(valid_loader)
+        for x, y in pbar:
+            pbar.set_description("Validando")
+            x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+
+            _, stft, wave_stft = stft_model(x)
+            wave = wave_model(x)
+            y_hat = network(stft, wave_stft, wave)
+
+            loss = mse_loss(y_hat, y)
+            batch_loss += loss.item() * y.size(0)
+            count += y.size(0)
+        return batch_loss / count
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", type=int, default=10, help="Tamaño del batch")
+    parser.add_argument("--channels", type=int, default=2, help="Número de canales de audio")
+    parser.add_argument("--checkpoint", type=str, help="Directorio de los checkpoints")
+    parser.add_argument("--dataset", type=str, default="musdb", choices=["musdb", "medleydb"], help="Nombre del dataset")
+    parser.add_argument("--duration", type=float, default=5.0, help="Duración de cada canción")
+    parser.add_argument("--epochs", type=int, default=10, help="Número de épocas")
+    parser.add_argument("--half", action="store_true", help="Partir canciones de validación por la mitad")
+    parser.add_argument("--learning-rate", type=float, default=0.001, help="Tasa de aprendizaje")
+    parser.add_argument("--output", type=str, help="Directorio de salida")
+    parser.add_argument("--path-stft", type=str, help="Ruta del modelo de STFT")
+    parser.add_argument("--path-wave", type=str, help="Ruta del modelo de Wave")
+    parser.add_argument("--root", type=str, help="Ruta del dataset")
+    parser.add_argument("--samples", type=int, default=1, help="Muestras por cancion")
+    parser.add_argument("--target", type=str, default="vocals", help="Instrumento a separar")
+    parser.add_argument("--weight-decay", type=float, default=0, help="Decaimiento de los pesos de Adam")
+    parser.add_argument("--workers", type=int, default=0, help="Número de workers para cargar los datos")
+
+    args = parser.parse_args()
+
+    torch.autograd.set_detect_anomaly(True)
+
+    use_cuda = torch.cuda.is_available()
+    print("GPU disponible:", use_cuda)
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    print("Cargando modelo de STFT")
+    stft_state = torch.load(f"{args.path_stft}/{args.target}/best_checkpoint", map_location=device)
+    stft_model = SpectrogramModel(*stft_state["args"]).to(device)
+    stft_model.eval()
+
+    print("Cargando modelo de Wave")
+    wave_state = torch.load(f"{args.path_wave}/{args.target}/best_checkpoint", map_location=device)
+    wave_model = WaveModel(*wave_state["args"]).to(device)
+    wave_mode.eval()
+
+    model_args = [stft_state["args"][0], stft_state["args"][-2], stft_state["args"][-1]]
+    network = BlendNet(*model_args).to(device)
+
+    if args.dataset == "musdb":
+        train_dataset = MUSDB18Dataset(base_path=args.root, subset="train", split="train", target=args.target,
+                                       duration=args.duration, samples=args.samples, random=True)
+        valid_dataset = MUSDB18Dataset(base_path=args.root, subset="train", split="valid", target=args.target,
+                                       duration=None, samples=1, random=False, half=args.half)
+    # elif args.dataset == "medleydb":
+    #     pass
+    else:
+        raise NotImplementedError
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=1, num_workers=args.workers, pin_memory=True)
+
+    optimizer = Adam(network.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    scheduler = ReduceLROnPlateau(optimizer, factor=0.5, patience=5, verbose=True)
+
+    if args.checkpoint:
+        state = torch.load(f"{args.checkpoint}/{args.target}/last_checkpoint", map_location=device)
+        network.load_state_dict(state["state_dict"])
+        optimizer.load_state_dict(state["optimizer"])
+        scheduler.load_state_dict(state["scheduler"])
+
+        train_losses = state["train_losses"]
+        valid_losses = state["valid_losses"]
+        initial_epoch = state["epoch"] + 1
+        best_loss = state["best_loss"]
+    else:
+        train_losses = []
+        valid_losses = []
+        initial_epoch = 1
+        best_loss = float("inf")
+
+    out_path = f"{args.output}/{args.target}"
+    os.makedirs(out_path, exist_ok=True)
+
+    t = tqdm.trange(initial_epoch, args.epochs + 1)
+    for epoch in t:
+        t.set_description("Entrenando iteración")
+        train_loss = train(network, train_loader, device, stft_model, wave_model, optimizer)
+        valid_loss = valid(network, valid_loader, device, stft_model, wave_model)
+        scheduler.step(valid_loss)
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
+
+        t.set_postfix(train_loss=train_loss, valid_loss=valid_loss)
+
+        state = {
+            "args": model_args
+            "epoch": epoch,
+            "best_loss": best_loss,
+            "state_dict": network.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "scheduler": scheduler.state_dict(),
+            "train_losses": train_losses,
+            "valid_losses": valid_losses
+        }
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            state["best_loss"] = best_loss
+            torch.save(state, f"{out_path}/best_checkpoint")
+        torch.save(state, f"{out_path}/last_checkpoint")
+
+if __name__ == '__main__':
+    main()
