14262043f574e96ce07eaf127868c9143922e0c5
Kajachuan <kevincajachuan@hotmail.com>
2021-05-30 13:19:50 -0300

    Cambio el modelo completo de mezcla

--- blend_net/blend.py
+++ blend_net/blend.py
@@ -23,13 +23,24 @@ class BlendNet(nn.Module):
 
         self.stft = STFT(self.nfft, self.hop)
 
-        # self.linear_mag = nn.Linear(in_features=self.bins * self.channels, out_features=self.bins * self.channels)
-        # self.prelu_mag = nn.PReLU(num_parameters=self.channels)
+        self.linear_stft = nn.Sequential(
+                               nn.Linear(in_features=2 * self.bins * self.channels, out_features=2 * self.bins),
+                               nn.PReLU(),
+                               nn.Linear(in_features=2 * self.bins, out_features=2),
+                               nn.Sigmoid()
+                           )
 
-        self.linear_wave = nn.Linear(in_features=2 * self.channels, out_features=self.channels)
+        self.linear_wave = nn.Sequential(
+                               nn.Linear(in_features=2 * self.channels, out_features=self.channels),
+                               nn.Tanh()
+                           )
 
-        self.linear_output = nn.Linear(in_features=4 * self.channels, out_features=self.channels)
-        self.tanh = nn.Tanh()
+        self.linear_output = nn.Sequential(
+                                 nn.Linear(in_features=4 * self.channels, out_features=2 * self.channels),
+                                 nn.Tanh(),
+                                 nn.Linear(in_features=2 * self.channels, out_features=self.channels),
+                                 nn.Tanh()
+                             )
 
     def forward(self, wave_stft: torch.Tensor, wave: torch.Tensor) -> torch.Tensor:
         """
@@ -41,35 +52,35 @@ class BlendNet(nn.Module):
             Separación de dimensión (n_batch, n_channels, timesteps)
         """
         # Mezcla con STFT
-        # stft = self.stft((wave_stft + wave) / 2)
-        # mag, phase = stft[..., 0], stft[..., 1]
-        # mag = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim = (n_batch, n_channels, n_bins, n_frames)
-        #
-        # mag = mag.transpose(1, 3) # Dim = (n_batch, n_frames, n_bins, n_channels)
-        # mag = mag.reshape(mag.size(0), mag.size(1), -1) # Dim = (n_batch, n_frames, n_bins * n_channels)
-        # mag = self.linear_mag(mag) # Dim = (n_batch, n_frames, n_bins * n_channels)
-        # mag = mag.reshape(mag.size(0), mag.size(1), -1, self.channels) # Dim = (n_batch, n_frames, n_bins, n_channels)
-        # mag = mag.transpose(1, 3) # Dim = (n_batch, n_channels, n_bins, n_frames)
-        # mag = self.prelu_mag(mag) # Dim = (n_batch, n_channels, n_bins, n_frames)
-        #
-        # estim_stft = torch.stack((mag * torch.cos(phase),
-        #                           mag * torch.sin(phase)), dim=-1)
-        # blend_stft = self.stft(estim_stft, inverse=True)
+        stft_stft = self.stft(wave_stft)
+        mag_stft, phase_stft = stft_stft[..., 0], stft_stft[..., 1]
+
+        stft_wave = self.stft(wave)
+        mag_wave, phase_wave = stft_wave[..., 0], stft_wave[..., 1]
+
+        data = torch.stack((mag_stft, mag_wave), dim=-1) # Dim = (n_batch, n_channels, n_bins, n_frames, 2)
+        data = data.transpose(1, 3) # Dim = (n_batch, n_frames, n_bins, n_channels, 2)
+        data = data.reshape(data.size(0), data.size(1), -1) # Dim = (n_batch, n_frames, n_bins * n_channels * 2)
+        data = self.linear_stft(data) # Dim = (n_batch, n_frames, 2)
+        data = data.reshape(data.size(0), 1, 1, data.size(1), 2) # Dim = (n_batch, 1, 1, n_frames, 2)
+
+        estim_stft = torch.stack((data[..., 0] * (mag_stft * torch.cos(phase_stft)) +
+                                  data[..., 1] * (mag_wave * torch.cos(phase_wave)),
+                                  data[..., 0] * (mag_stft * torch.sin(phase_stft)) +
+                                  data[..., 1] * (mag_wave * torch.sin(phase_wave))), dim=-1)
+        blend_stft = self.stft(estim_stft, inverse=True)
 
         # Mezcla con Wave
         data = torch.stack([wave_stft, wave], dim=-1) # Dim = (n_batch, n_channels, timesteps, 2)
         data = data.transpose(1, 2) # Dim = (n_batch, timesteps, n_channels, 2)
         data = data.reshape(data.size(0), data.size(1), -1) # Dim = (n_batch, timesteps, n_channels * 2)
         data = self.linear_wave(data) # Dim = (n_batch, timesteps, n_channels)
-        data = data.transpose(1, 2) # Dim = (n_batch, n_channels, timesteps)
-        blend_wave = self.tanh(data) # Dim = (n_batch, n_channels, timesteps)
+        blend_wave = data.transpose(1, 2) # Dim = (n_batch, n_channels, timesteps)
 
         # Mezclo todo
-        blend_stft = blend_wave # Borrar después
         data = torch.stack([wave_stft, wave, blend_stft, blend_wave], dim=-1) # Dim = (n_batch, n_channels, timesteps, 4)
         data = data.transpose(1, 2) # Dim = (n_batch, timesteps, n_channels, 4)
         data = data.reshape(data.size(0), data.size(1), -1) # Dim = (n_batch, timesteps, n_channels * 4)
         data = self.linear_output(data) # Dim = (n_batch, timesteps, n_channels)
         data = data.transpose(1, 2) # Dim = (n_batch, n_channels, timesteps)
-        data = self.tanh(data) # Dim = (n_batch, n_channels, timesteps)
         return data
