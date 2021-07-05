3d84cce2ea13419b97656213c803a80a1ce73539
Kajachuan <kevincajachuan@hotmail.com>
2021-07-05 20:21:37 -0300

    Comento los modelos anteriores

--- train_blend.py
+++ train_blend.py
@@ -83,19 +83,21 @@ def main():
 
     print("Cargando modelo de STFT")
     stft_state = torch.load(f"{args.path_stft}/{args.target}/best_checkpoint", map_location=device)
-    stft_model = SpectrogramModel(*stft_state["args"]).to(device)
-    stft_model.load_state_dict(stft_state["state_dict"])
-    stft_model.eval()
-    for param in stft_model.parameters():
-        param.requires_grad = False
+    # stft_model = SpectrogramModel(*stft_state["args"]).to(device)
+    # stft_model.load_state_dict(stft_state["state_dict"])
+    # stft_model.eval()
+    # for param in stft_model.parameters():
+    #     param.requires_grad = False
+    stft_model = None
 
-    print("Cargando modelo de Wave")
-    wave_state = torch.load(f"{args.path_wave}/{args.target}/best_checkpoint", map_location=device)
-    wave_model = WaveModel(*wave_state["args"]).to(device)
-    wave_model.load_state_dict(wave_state["state_dict"])
-    wave_model.eval()
-    for param in wave_model.parameters():
-        param.requires_grad = False
+    # print("Cargando modelo de Wave")
+    # wave_state = torch.load(f"{args.path_wave}/{args.target}/best_checkpoint", map_location=device)
+    # wave_model = WaveModel(*wave_state["args"]).to(device)
+    # wave_model.load_state_dict(wave_state["state_dict"])
+    # wave_model.eval()
+    # for param in wave_model.parameters():
+    #     param.requires_grad = False
+    wave_model = None
 
     model_args = [args.layers_spec, args.layers_wave, stft_state["args"][0], stft_state["args"][-2],
                   stft_state["args"][-1], args.activation, args.wave_type]
