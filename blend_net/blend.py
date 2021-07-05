9456bf53188eaa4ac011f5e9a2920c9a4177a08a
Kajachuan <kevincajachuan@hotmail.com>
2021-07-05 20:08:43 -0300

    Cambio el nombre de la variable

--- blend_net/blend.py
+++ blend_net/blend.py
@@ -175,6 +175,6 @@ class BlendNet(nn.Module):
         data = data.reshape(data.size(0), data.size(1), self.channels, -1) # Dim = (n_batch, timesteps, n_channels, 3)
         data = data.transpose(1, 3) # Dim = (n_batch, 3, n_channels, timesteps)
         data = self.activation(data) # Dim = (n_batch, 3, n_channels, timesteps)
-        data *= input
-        data = data.sum(1)
-        return data
+        res = data * input
+        res = res.sum(1)
+        return res
