9dac89ff4d48b93995713e4636b1dabf903b171f
Kajachuan <kevincajachuan@hotmail.com>
2021-07-06 12:19:02 -0300

    Cambio variables

--- blend_net/blend.py
+++ blend_net/blend.py
@@ -21,7 +21,8 @@ class BlendNet(nn.Module):
         # mag, phase = stft[..., 0], stft[..., 1]
         # mag_db = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim: (n_batch, n_channels, n_bins, n_frames)
         # Dim = (n_batch, n_channels, timesteps)
-        data = input.transpose(1, 2) # Dim: (n_batch, timesteps, n_channels)
+        mag = input
+        data = mag.transpose(1, 2) # Dim: (n_batch, timesteps, n_channels)
         # data = data.reshape(data.size(0), data.size(1), -1) # Dim: (n_batch, n_frames, n_bins * n_channels)
         self.blstm.flatten_parameters()
         data = self.blstm(data)[0] # Dim: (n_batch, timesteps, hidden)
@@ -30,7 +31,7 @@ class BlendNet(nn.Module):
         data = data.transpose(1, 2) # Dim: (n_batch, n_channels, timesteps)
         mask = self.activation(data)
 
-        estim = input * mask
+        estim = mag * mask
         # estim_stft = torch.stack((estim_mag * torch.cos(phase),
         #                           estim_mag * torch.sin(phase)), dim=-1)
         # estimates = self.stft(estim_stft, inverse=True)
