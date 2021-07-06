7ee46b04de36cf6abfbd9214a0eb90a195a96137
Kajachuan <kevincajachuan@hotmail.com>
2021-07-06 11:57:12 -0300

    Cambio para procesar en tiempo

--- blend_net/blend.py
+++ blend_net/blend.py
@@ -11,26 +11,27 @@ class BlendNet(nn.Module):
         self.hop = hop
         self.channels = channels
         self.stft = STFT(nfft, hop)
-        self.blstm = nn.LSTM(self.bins * channels, 64, 2, batch_first=True,
-                             dropout=0.3, bidirectional=True)
-        self.linear = nn.Linear(128, self.bins * channels)
+        self.blstm = nn.LSTM(input_size=channels, hidden_size=64, num_layers=2,
+                             batch_first=True, dropout=0.3, bidirectional=True)
+        self.linear = nn.Linear(128, channels)
         self.activation = nn.Sigmoid()
 
-    def forward(self, data: torch.Tensor) -> torch.Tensor:
-        stft = self.stft(data)
-        mag, phase = stft[..., 0], stft[..., 1]
-        mag_db = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim: (n_batch, n_channels, n_bins, n_frames)
-        data = mag_db.transpose(1, 3) # Dim: (n_batch, n_frames, n_bins, n_channels)
-        data = data.reshape(data.size(0), data.size(1), -1) # Dim: (n_batch, n_frames, n_bins * n_channels)
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        # stft = self.stft(data)
+        # mag, phase = stft[..., 0], stft[..., 1]
+        # mag_db = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim: (n_batch, n_channels, n_bins, n_frames)
+        # Dim = (n_batch, n_channels, timesteps)
+        data = input.transpose(1, 2) # Dim: (n_batch, timesteps, n_channels)
+        # data = data.reshape(data.size(0), data.size(1), -1) # Dim: (n_batch, n_frames, n_bins * n_channels)
         self.blstm.flatten_parameters()
-        data = self.blstm(data)[0] # Dim: (n_batch, n_frames, hidden)
-        data = self.linear(data) # Dim: (n_batch, n_frames, n_bins * n_channels)
-        data = data.reshape(data.size(0), data.size(1), self.bins, self.channels) # Dim: (n_batch, n_frames, n_bins, n_channels)
-        data = data.transpose(1, 3) # Dim: (n_batch, n_channels, n_bins, n_frames)
+        data = self.blstm(data)[0] # Dim: (n_batch, timesteps, hidden)
+        data = self.linear(data) # Dim: (n_batch, timesteps, n_channels)
+        # data = data.reshape(data.size(0), data.size(1), self.bins, self.channels) # Dim: (n_batch, n_frames, n_bins, n_channels)
+        data = data.transpose(1, 2) # Dim: (n_batch, n_channels, timesteps)
         mask = self.activation(data)
 
-        estim_mag = mag * mask
-        estim_stft = torch.stack((estim_mag * torch.cos(phase),
-                                  estim_mag * torch.sin(phase)), dim=-1)
-        estimates = self.stft(estim_stft, inverse=True)
-        return estimates
+        estim = input * mask
+        # estim_stft = torch.stack((estim_mag * torch.cos(phase),
+        #                           estim_mag * torch.sin(phase)), dim=-1)
+        # estimates = self.stft(estim_stft, inverse=True)
+        return estim
