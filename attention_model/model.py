18126dd37f716374ce904f4a28333fa81341c013
Kajachuan <kevincajachuan@hotmail.com>
2021-08-10 14:37:23 -0300

    Nuevo modelo con atención

---
+++ attention_model/model.py
@@ -0,0 +1,156 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.stft import STFT
+
+def pos_encoding(embed_dim, length):
+    pos_enc = torch.zeros(length, embed_dim)
+    pos = torch.arange(0, length).unsqueeze(1)
+    div = torch.exp(torch.arange(0, embed_dim, 2).float() * (-math.log(10000.0) / embed_dim))
+    pos_enc[:, 0::2] = torch.sin(pos * div)
+    pos_enc[:, 1::2] = torch.cos(pos * div)
+    return pos_enc.unsqueeze(0)
+
+def zero_pad(x, skip):
+    diff = skip.size(-1) - x.size(-1)
+    l_pad = diff // 2
+    r_pad = diff - l_pad
+    x = F.pad(x, (l_pad, r_pad), 'constant', 0)
+    return x
+
+class Encoder(nn.Module):
+    def __init__(self, nfft: int, hop: int, heads: int, dropout: float,
+                 in_channels: int, out_channels: int) -> None:
+        super(Encoder, self).__init__()
+        self.bins = nfft // 2 + 1
+        self.stft = STFT(nfft, hop)
+        self.attention = nn.MultiheadAttention(embed_dim=self.bins * in_channels, num_heads=heads, 
+                                               dropout=dropout, batch_first=True)
+        self.norm_att = nn.LayerNorm(normalized_shape=self.bins * in_channels)
+        self.linear = nn.Linear(in_features=self.bins * in_channels, out_features=self.bins * in_channels)
+        self.norm_linear = nn.LayerNorm(normalized_shape=self.bins * in_channels)
+        self.glu = nn.GLU(dim=1)
+        self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels,
+                              kernel_size=8, stride=4)
+        self.relu = nn.LeakyReLU()
+
+    def forward(self, wave: torch.Tensor) -> torch.Tensor:
+        '''
+        Argumentos:
+            wave -- Tensor de dimensión (batch, in_channels, timesteps)
+        '''
+        # 1° Parte: Frecuencia
+        stft = self.stft(wave)
+        mag, phase = stft[..., 0], stft[..., 1]
+        mag_db = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim: (batch, in_channels, bins, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, bins, in_channels, frames)
+        mag_db = mag_db.reshape(mag_db.size(0), -1, mag_db.size(-1)) # Dim: (batch, bins * in_channels, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, frames, bins * in_channels)
+        mag_db = mag_db + pos_encoding(mag_db.size(-1), mag_db.size(1))
+
+        mag = self.attention(mag_db, mag_db, mag_db)
+        mag = mag + mag_db
+        skip = self.norm_att(mag)
+        mag = self.linear(skip)
+        mag = mag + skip
+        mag = self.norm_linear(mag)
+
+        mag_db = mag.transpose(1, 2) # Dim: (batch, bins * in_channels, frames)
+        mag_db = mag_db.reshape(mag_db.size(0), self.bins, -1, mag_db.size(-1)) # Dim: (batch, bins, in_channels, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, in_channels, bins, frames)
+        stft = torch.stack((mag_db * torch.cos(phase), mag_db * torch.sin(phase)), dim=-1)
+        new_wave = self.stft(stft, inverse=True) # Dim: (batch, in_channels, timesteps)
+        wave = torch.cat((wave, new_wave), dim=1) # Dim: (batch, 2 * in_channels, timesteps)
+        wave = self.glu(wave) # Dim: (batch, in_channels, timesteps)
+
+        # 2° Parte: Tiempo
+        wave = self.conv(wave) # Dim: (batch, out_channels, timesteps)
+        wave = self.relu(wave)
+        return wave, mag
+
+class Decoder(nn.Module):
+    def __init__(self, nfft: int, hop: int, heads: int, dropout: float,
+                 in_channels: int, out_channels: int) -> None:
+        super(Decoder, self).__init__()
+        self.bins = nfft // 2 + 1
+        self.stft = STFT(nfft, hop)
+        self.attention = nn.MultiheadAttention(embed_dim=self.bins * out_channels, num_heads=heads, 
+                                               dropout=dropout, batch_first=True)
+        self.norm_att = nn.LayerNorm(normalized_shape=self.bins * out_channels)
+        self.linear = nn.Linear(in_features=self.bins * out_channels, out_features=self.bins * out_channels)
+        self.norm_linear = nn.LayerNorm(normalized_shape=self.bins * out_channels)
+        self.glu = nn.GLU(dim=1)
+        self.conv = nn.ConvTranspose1d(in_channels=in_channels, out_channels=out_channels,
+                              kernel_size=8, stride=4)
+        self.relu = nn.LeakyReLU()
+
+
+    def forward(self, wave: torch.Tensor, mag_skip: torch.Tensor) -> torch.Tensor:
+        '''
+        Argumentos:
+            wave -- Tensor proveniente del decoder anterior de dimensión (batch, in_channels, timesteps)
+            mag_skip -- Tensor del STFT del encoder de dimensión (batch, frames, bins * out_channels)
+        '''
+        # 1° Parte: Tiempo
+        wave = self.conv(wave) # Dim: (batch, out_channels, timesteps)
+        wave = self.relu(wave)
+
+        # 2° Parte: Frecuencia
+        stft = self.stft(wave)
+        mag, phase = stft[..., 0], stft[..., 1]
+        mag_db = 10 * torch.log10(torch.clamp(mag, min=1e-8)) # Dim: (batch, out_channels, bins, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, bins, out_channels, frames)
+        mag_db = mag_db.reshape(mag_db.size(0), -1, mag_db.size(-1)) # Dim: (batch, bins * out_channels, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, frames, bins * out_channels)
+        mag_db = mag_db + pos_encoding(mag_db.size(-1), mag_db.size(1))
+
+        mag = self.attention(mag_skip, mag_skip, mag_db)
+        mag = mag + mag_db
+        skip = self.norm_att(mag)
+        mag = self.linear(skip)
+        mag = mag + skip
+        mag = self.norm_linear(mag)
+
+        mag_db = mag.transpose(1, 2) # Dim: (batch, bins * out_channels, frames)
+        mag_db = mag_db.reshape(mag_db.size(0), self.bins, -1, mag_db.size(-1)) # Dim: (batch, bins, out_channels, frames)
+        mag_db = mag_db.transpose(1, 2) # Dim: (batch, out_channels, bins, frames)
+        stft = torch.stack((mag_db * torch.cos(phase), mag_db * torch.sin(phase)), dim=-1)
+        new_wave = self.stft(stft, inverse=True) # Dim: (batch, out_channels, timesteps)
+        wave = torch.cat((wave, new_wave), dim=1) # Dim: (batch, 2 * out_channels, timesteps)
+        wave = self.glu(wave) # Dim: (batch, out_channels, timesteps)
+        return wave
+
+class AttentionModel(nn.Module):
+    def __init__(self, layers: int, nfft: int, hop: int, dropout: float) -> None:
+        super(AttentionModel, self).__init__()
+        self.encoder = nn.ModuleList([Encoder(nfft=nfft, hop=hop, 
+                                              heads=2**i if i < 3 else 8, dropout=dropout, 
+                                              in_channels=2**i, out_channels=2**(i+1))
+                                              for i in range(1, layers + 1)])
+        self.decoder = nn.ModuleList([Decoder(nfft=nfft, hop=hop,
+                                              heads=2**i if i < 3 else 8, dropout=dropout,
+                                              in_channels=2**(i+1), out_channels=2**i)
+                                              for i in range(layers, 0, -1)])
+
+    def forward(self, mix: torch.Tensor) -> torch.Tensor:
+        x = mix
+        mono = mix.mean(dim=1, keepdim=True)
+        mean = mono.mean(dim=-1, keepdim=True)
+        std = mono.std(dim=-1, keepdim=True)
+
+        x = (x - mean) / (1e-8 + std)
+
+        skips = []
+        for encode in self.encoder:
+            x, mag_skip = encode(x)
+            skips.append((x, mag_skip))
+        
+        for decode in self.decoder:
+            wave_skip, mag_skip = skips.pop(-1)
+            x = zero_pad(x, wave_skip)
+            x = x + wave_skip
+            x = decode(x, mag_skip)
+
+        x = x * std + mean
+        return x
---
+++ train_attention.py
@@ -0,0 +1,137 @@
+import argparse
+import os
+import torch
+import tqdm
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from torch.nn.functional import mse_loss
+from dataset.dataset import MUSDB18Dataset
+from attention_model.model import AttentionModel
+
+def train(network, train_loader, device, optimizer):
+    batch_loss, count = 0, 0
+    network.train()
+    pbar = tqdm.tqdm(train_loader)
+    for x, y in pbar:
+        pbar.set_description("Entrenando batch")
+        x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+        optimizer.zero_grad()
+        y_hat = network(x)
+        loss = mse_loss(y_hat, y)
+        loss.backward()
+        optimizer.step()
+        batch_loss += loss.item() * y.size(0)
+        count += y.size(0)
+    return batch_loss / count
+
+def valid(network, valid_loader, device):
+    batch_loss, count = 0, 0
+    network.eval()
+    with torch.no_grad():
+        pbar = tqdm.tqdm(valid_loader)
+        for x, y in pbar:
+            pbar.set_description("Validando")
+            x, y = x.to(device, non_blocking=True), y.to(device, non_blocking=True)
+            y_hat = network(x)
+            loss = mse_loss(y_hat, y)
+            batch_loss += loss.item() * y.size(0)
+            count += y.size(0)
+        return batch_loss / count
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", type=int, default=10, help="Tamaño del batch")
+    parser.add_argument("--checkpoint", type=str, help="Directorio de los checkpoints")
+    parser.add_argument("--dataset", type=str, default="musdb", choices=["musdb", "medleydb"], help="Nombre del dataset")
+    parser.add_argument("--dropout", type=float, default=0.3, help="Dropout del Multihead Attention")
+    parser.add_argument("--duration", type=float, default=5.0, help="Duración de cada canción")
+    parser.add_argument("--epochs", type=int, default=10, help="Número de épocas")
+    parser.add_argument("--hop", type=int, default=1024, help="Tamaño del hop del STFT")
+    parser.add_argument("--layers", type=int, default=5, help="Número de capas")
+    parser.add_argument("--learning-rate", type=float, default=0.001, help="Tasa de aprendizaje")
+    parser.add_argument("--nfft", type=int, default=4096, help="Tamaño de la FFT del STFT")
+    parser.add_argument("--output", type=str, help="Directorio de salida")
+    parser.add_argument("--partitions", type=int, default=1, help="Número de partes de las canciones de validación")
+    parser.add_argument("--root", type=str, help="Ruta del dataset")
+    parser.add_argument("--samples", type=int, default=1, help="Muestras por cancion")
+    parser.add_argument("--target", type=str, default="vocals", help="Instrumento a separar")
+    parser.add_argument("--weight-decay", type=float, default=0, help="Decaimiento de los pesos de Adam")
+    parser.add_argument("--workers", type=int, default=0, help="Número de workers para cargar los datos")
+
+    args = parser.parse_args()
+
+    torch.autograd.set_detect_anomaly(True)
+
+    use_cuda = torch.cuda.is_available()
+    print("GPU disponible:", use_cuda)
+    device = torch.device("cuda:0" if use_cuda else "cpu")
+
+    model_args = [args.layers, args.nfft, args.hop, args.dropout]
+    network = AttentionModel(*model_args).to(device)
+
+    if args.dataset == "musdb":
+        train_dataset = MUSDB18Dataset(base_path=args.root, subset="train", split="train", target=args.target,
+                                       duration=args.duration, samples=args.samples, random=True)
+        valid_dataset = MUSDB18Dataset(base_path=args.root, subset="train", split="valid", target=args.target,
+                                       duration=None, samples=1, random=False, partitions=args.partitions)
+    # elif args.dataset == "medleydb":
+    #     pass
+    else:
+        raise NotImplementedError
+
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=1, num_workers=args.workers, pin_memory=True)
+
+    optimizer = Adam(network.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+    scheduler = ReduceLROnPlateau(optimizer, factor=0.5, patience=5, verbose=True)
+
+    if args.checkpoint:
+        state = torch.load(f"{args.checkpoint}/{args.target}/last_checkpoint", map_location=device)
+        network.load_state_dict(state["state_dict"])
+        optimizer.load_state_dict(state["optimizer"])
+        scheduler.load_state_dict(state["scheduler"])
+
+        train_losses = state["train_losses"]
+        valid_losses = state["valid_losses"]
+        initial_epoch = state["epoch"] + 1
+        best_loss = state["best_loss"]
+    else:
+        train_losses = []
+        valid_losses = []
+        initial_epoch = 1
+        best_loss = float("inf")
+
+    out_path = f"{args.output}/{args.target}"
+    os.makedirs(out_path, exist_ok=True)
+
+    t = tqdm.trange(initial_epoch, args.epochs + 1)
+    for epoch in t:
+        t.set_description("Entrenando iteración")
+        train_loss = train(network, train_loader, device, optimizer)
+        valid_loss = valid(network, valid_loader, device)
+        scheduler.step(valid_loss)
+        train_losses.append(train_loss)
+        valid_losses.append(valid_loss)
+
+        t.set_postfix(train_loss=train_loss, valid_loss=valid_loss)
+
+        state = {
+            "args": model_args,
+            "epoch": epoch,
+            "best_loss": best_loss,
+            "state_dict": network.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "scheduler": scheduler.state_dict(),
+            "train_losses": train_losses,
+            "valid_losses": valid_losses
+        }
+
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            state["best_loss"] = best_loss
+            torch.save(state, f"{out_path}/best_checkpoint")
+        torch.save(state, f"{out_path}/last_checkpoint")
+
+if __name__ == '__main__':
+    main()
